55028302a009ecc1fdffd5b59c40d481367de50f
Alexandre Bury <alexandre.bury@gmail.com>
2022-04-25 16:30:54 -0400

    Add max-open-files to partition command

--- src/cmd/partition.rs
+++ src/cmd/partition.rs
@@ -1,5 +1,5 @@
-use std::collections::{HashMap, HashSet};
 use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
 use std::io;
 use std::path::Path;
@@ -7,10 +7,10 @@ use std::path::Path;
 use csv;
 use regex::Regex;
 
-use CliResult;
 use config::{Config, Delimiter};
 use select::SelectColumns;
 use util::{self, FilenameTemplate};
+use CliResult;
 
 static USAGE: &'static str = "
 Partitions the given CSV data into chunks based on the value of a column
@@ -32,6 +32,8 @@ partition options:
                            specified number of bytes when creating the
                            output file.
     --drop                 Drop the partition column from results.
+    --max-open-files <n>   Maximum number of files to keep open.
+                           [default: 512]
 
 Common options:
     -h, --help             Display this message
@@ -47,6 +49,7 @@ struct Args {
     arg_column: SelectColumns,
     arg_input: Option<String>,
     arg_outdir: String,
+    flag_max_open_files: usize,
     flag_filename: FilenameTemplate,
     flag_prefix_length: Option<usize>,
     flag_drop: bool,
@@ -75,11 +78,7 @@ impl Args {
     }
 
     /// Get the column to use as a key.
-    fn key_column(
-        &self,
-        rconfig: &Config,
-        headers: &csv::ByteRecord,
-    ) -> CliResult<usize> {
+    fn key_column(&self, rconfig: &Config, headers: &csv::ByteRecord) -> CliResult<usize> {
         let select_cols = rconfig.selection(headers)?;
         if select_cols.len() == 1 {
             Ok(select_cols[0])
@@ -94,11 +93,23 @@ impl Args {
         let mut rdr = rconfig.reader()?;
         let headers = rdr.byte_headers()?.clone();
         let key_col = self.key_column(&rconfig, &headers)?;
-        let mut gen = WriterGenerator::new(self.flag_filename.clone());
 
-        let mut writers: HashMap<Vec<u8>, BoxedWriter> =
-            HashMap::new();
+        let seen_keys = HashSet::new();
+        let mut writers = lru::LruCache::new(self.flag_max_open_files);
+        let gen = WriterGenerator::new(self.flag_filename.clone());
+
+        let mut pool = WriterPool {
+            seen_keys,
+            gen,
+            rconfig,
+            outdir: self.arg_outdir.clone(),
+            flag_drop: self.flag_drop,
+            headers,
+            key_col,
+        };
+
         let mut row = csv::ByteRecord::new();
+
         while rdr.read_byte_record(&mut row)? {
             // Decide what file to put this in.
             let column = &row[key_col];
@@ -107,26 +118,15 @@ impl Args {
                 Some(len) if len < column.len() => &column[0..len],
                 _ => &column[..],
             };
-            let mut entry = writers.entry(key.to_vec());
-            let wtr = match entry {
-                Entry::Occupied(ref mut occupied) => occupied.get_mut(),
-                Entry::Vacant(vacant) => {
-                    // We have a new key, so make a new writer.
-                    let mut wtr = gen.writer(&*self.arg_outdir, key)?;
-                    if !rconfig.no_headers {
-                        if self.flag_drop {
-                            wtr.write_record(headers.iter().enumerate()
-                                .filter_map(|(i, e)| if i != key_col { Some(e) } else { None }))?;
-                        } else {
-                            wtr.write_record(&headers)?;
-                        }
-                    }
-                    vacant.insert(wtr)
-                }
-            };
+            let wtr = pool.writer(&mut writers, key)?;
             if self.flag_drop {
-                wtr.write_record(row.iter().enumerate()
-                    .filter_map(|(i, e)| if i != key_col { Some(e) } else { None }))?;
+                wtr.write_record(row.iter().enumerate().filter_map(|(i, e)| {
+                    if i != key_col {
+                        Some(e)
+                    } else {
+                        None
+                    }
+                }))?;
             } else {
                 wtr.write_byte_record(&row)?;
             }
@@ -135,7 +135,7 @@ impl Args {
     }
 }
 
-type BoxedWriter = csv::Writer<Box<io::Write+'static>>;
+type BoxedWriter = csv::Writer<Box<io::Write + 'static>>;
 
 /// Generates unique filenames based on CSV values.
 struct WriterGenerator {
@@ -148,7 +148,7 @@ struct WriterGenerator {
 impl WriterGenerator {
     fn new(template: FilenameTemplate) -> WriterGenerator {
         WriterGenerator {
-            template: template,
+            template,
             counter: 1,
             used: HashSet::new(),
             non_word_char: Regex::new(r"\W").unwrap(),
@@ -157,7 +157,17 @@ impl WriterGenerator {
 
     /// Create a CSV writer for `key`.  Does not add headers.
     fn writer<P>(&mut self, path: P, key: &[u8]) -> io::Result<BoxedWriter>
-        where P: AsRef<Path>
+    where
+        P: AsRef<Path>,
+    {
+        let unique_value = self.unique_value(key);
+        self.template.writer(path.as_ref(), &unique_value)
+    }
+
+    /// Create a CSV writer for `key`.  Does not add headers. Does not truncate file.
+    fn re_open<P>(&mut self, path: P, key: &[u8]) -> io::Result<BoxedWriter>
+    where
+        P: AsRef<Path>,
     {
         let unique_value = self.unique_value(key);
         self.template.writer(path.as_ref(), &unique_value)
@@ -170,12 +180,11 @@ impl WriterGenerator {
         // Sanitize our key.
         let utf8 = String::from_utf8_lossy(key);
         let safe = self.non_word_char.replace_all(&*utf8, "").into_owned();
-        let base =
-            if safe.is_empty() {
-                "empty".to_owned()
-            } else {
-                safe
-            };
+        let base = if safe.is_empty() {
+            "empty".to_owned()
+        } else {
+            safe
+        };
 
         // Now check for collisions.
         if !self.used.contains(&base) {
@@ -192,9 +201,59 @@ impl WriterGenerator {
                 });
                 if !self.used.contains(&candidate) {
                     self.used.insert(candidate.clone());
-                    return candidate
+                    return candidate;
                 }
             }
         }
+    }
+}
+
+struct WriterPool {
+    seen_keys: HashSet<Vec<u8>>,
+    outdir: String,
+    gen: WriterGenerator,
+    rconfig: Config,
+    flag_drop: bool,
+    headers: csv::ByteRecord,
+    key_col: usize,
+}
+
+impl WriterPool {
+    fn new_writer(&mut self, key: &[u8]) -> io::Result<BoxedWriter> {
+        if self.seen_keys.contains(key) {
+            // We have seen this file before; just re-open it.
+            self.gen.re_open(&*self.outdir, key)
+        } else {
+            self.seen_keys.insert(key.to_vec());
+            // Need a new writer
+            let mut wtr = self.gen.writer(&*self.outdir, key)?;
+            if !self.rconfig.no_headers {
+                if self.flag_drop {
+                    wtr.write_record(self.headers.iter().enumerate().filter_map(|(i, e)| {
+                        if i != self.key_col {
+                            Some(e)
+                        } else {
+                            None
+                        }
+                    }))?;
+                } else {
+                    wtr.write_record(&self.headers)?;
+                }
+            }
+            Ok(wtr)
+        }
+    }
+
+    fn writer<'a>(
+        &mut self,
+        writers: &'a mut lru::LruCache<Vec<u8>, BoxedWriter>,
+        key: &[u8],
+    ) -> io::Result<&'a mut BoxedWriter> {
+        // TODO: LRU doesn't currently have an Entry API to avoid computing hash 3 times.
+        if !writers.contains(key) {
+            writers.put(key.to_vec(), self.new_writer(key)?);
+        }
+
+        return Ok(writers.get_mut(key).unwrap());
     }
 }
